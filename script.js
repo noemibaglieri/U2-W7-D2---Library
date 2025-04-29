de248c02fd99a401f873068642c782cb8ba28aa7
Noemi Baglieri <noemibaglieri@gmail.com>
2025-04-29 16:08:13 +0200

    added card styles

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const fetchBooks = () => {
 
         const card = document.createElement("div");
         card.className = "card";
+        card.style.height = "40rem";
 
         const img = document.createElement("img");
         img.style.height = "400px";
@@ -22,21 +23,21 @@ const fetchBooks = () => {
         img.src = book.img;
 
         const cardBody = document.createElement("div");
-        cardBody.className = "card-body";
+        cardBody.className = "card-body d-flex flex-column";
 
         const cardTitle = document.createElement("h5");
         cardTitle.className = "card-title";
         cardTitle.innerText = book.title;
 
         const cardPrice = document.createElement("p");
-        cardPrice.className = "card-text";
+        cardPrice.className = "card-text flex-grow-1";
         cardPrice.innerText = "£" + book.price;
 
         const cardBtn = document.createElement("a");
         cardBtn.href = "#";
-        cardBtn.className = "btn text-white";
-        cardBtn.style.backgroundColor = "#A419A4";
-        cardBtn.style.borderColor = "#5B0870";
+        cardBtn.className = "btn text-white fw-semibold align-self-start";
+        cardBtn.style.border = "none";
+        cardBtn.style.backgroundColor = "#380258";
         cardBtn.innerText = "Scarta";
 
         col.appendChild(card);
@@ -48,7 +49,7 @@ const fetchBooks = () => {
         row.appendChild(col);
 
         cardBtn.onclick = function (e) {
-          e.target.closest(".card").remove();
+          e.target.closest(".col-md-3").remove();
         };
       });
     })
